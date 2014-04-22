c517915ec782225f7e502edf4a306c7e9a3f61fa
Kun.G <nukoug@gmail.com>
2014-04-22 17:06:30 +0800

    fix typoes

--- src/index.js
+++ src/index.js
@@ -64,6 +64,141 @@ var config = {
 //  }
 //});
 
+function paymentHandler (request, response) {
+  if (request.url.substr(0, 5) === '/pay?') {
+    var ppKey = '-----BEGIN PUBLIC KEY-----\n' +
+      'MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEArjB/MQESS9k2Xejv0yN8\n'+
+      't3qQ+IO8UqRTNeqgE4GoPBzmyPY4XJVcijAm7eHkUaF6yxGDh8D03R/yhrQBSdJ1\n'+
+      'GHLEew3KH3VgKNigdN9LGfJuH+k6JgCHwVK+diEQCzkhF6D7qrDvLNkF5iNQr2D+\n'+
+      '3lDAoAhE/PbYvJTH6KGIwx+TJtnNjJMhdE8WmWY5/3OAgyFU6DwVJs4phYWwZsYY\n'+
+      '+Z8s6sq7xKjYeCS0vVonNZ6m9VnYhv1NknxbHDovOMY3Ix/hsu+g2YiJFuUlyyaR\n'+
+      'bM4NUbZfu9OcfAGMo0L2vl6x/x7WzWBRKIGO7ONELfeit3PEPxE0kBvunAMPkQb5\n'+
+      'cwIDAQAB\n'+
+      '-----END PUBLIC KEY-----';
+    var data = new Buffer(0);
+    request.on('data', function (chunk) { data = Buffer.concat([data, chunk]); });
+    request.on('end', function (chunk) {
+      data = 'pay?'+data.toString();
+      var out = urlLib.parse(data, true).query;
+      if (out.sign) {
+        var cipher = rsaLib.createPublicKey(ppKey);
+        var info = cipher.publicDecrypt(new Buffer(out.sign, 'base64'), null, 'utf8');
+        info = JSON.parse(info);
+        if (out.order_id === info.order_id && out.amount === info.amount) {
+          var receipt = info.billno;
+          var receiptInfo = unwrapReceipt(receipt);
+          var serverName = 'Master'; //TODO:多服的情况?
+          dbWrapper.updateReceipt(receipt, RECEIPT_STATE_AUTHORIZED, function (err) {
+            dbLib.deliverMessage(receiptInfo.name, {
+              type: MESSAGE_TYPE_ChargeDiamond,
+              paymentType: 'PP25',
+              receipt: receipt
+            }, function (err, messageID) {
+              dbWrapper.updateReceipt(receipt, RECEIPT_STATE_DELIVERED, function () {});
+            }, serverName);
+
+            if (err === null) {
+              logInfo({action: 'AcceptPayment', receipt: receipt, info: info});
+              return response.end('success');
+            } else {
+              logError({action: 'AcceptPayment', error:err, data: data});
+              response.end('fail');
+            }
+          });
+        }
+      }
+      data = new Buffer(0);
+    });
+    request.on('error', function (err) {
+      logError({action: 'AcceptPayment', error:err, data: data});
+      response.end('fail');
+    });
+  } else if (request.url.substr(0, 5) === '/911?') {
+    out = urlLib.parse(request.url, true).query;
+    var appKey = 'd30d9f0f53e2654274505e25c27913fe709eb1ad6265e5c5';
+    var sign = out.AppId+out.Act+out.ProductName+out.ConsumeStreamId+out.CooOrderSerial+
+      out.Uin+out.GoodsId+out.GoodsInfo+out.GoodsCount+out.OriginalMoney+out.OrderMoney+
+      out.Note+out.PayStatus+out.CreateTime+appKey;
+    var b = new Buffer(1024);
+    var len = b.write(sign);
+    sign = md5Hash(b.toString('binary', 0, len));
+    if (sign === out.Sign) {
+      var receipt = out.CooOrderSerial;
+      var receiptInfo = unwrapReceipt(receipt);
+      var serverName = 'Master'; //TODO:多服的情况?
+      dbWrapper.updateReceipt(receipt, RECEIPT_STATE_AUTHORIZED, function (err) {
+        dbLib.getPlayerNameByID(receiptInfo.id, serverName, function (err, name) {
+          dbLib.deliverMessage(name, {
+            type: MESSAGE_TYPE_ChargeDiamond,
+            paymentType: 'ND91',
+            receipt: receipt
+          }, function (err, messageID) {
+            dbWrapper.updateReceipt(receipt, RECEIPT_STATE_DELIVERED, function () {});
+          }, serverName);
+          if (err === null) {
+            logInfo({action: 'AcceptPayment', receipt: receipt, info: out, receiptInfo: receiptInfo, name: name});
+            return response.end('{"ErrorCode": "1", "ErrorDesc": "OK"}');
+          } else {
+            logError({action: 'AcceptPayment', error:err, info: out, receiptInfo: receiptInfo, name: name});
+            return response.end('{"ErrorCode": "0", "ErrorDesc": "Fail"}');
+          }
+        });
+      });
+    } else {
+      logError({action: 'AcceptPayment', error: 'SignMissmatch', info: out, sign: sign});
+      response.end('{"ErrorCode": "5", "ErrorDesc": "Fail"}');
+    }
+  } else if (request.url.substr(0, 4) === '/kyp') {
+    var kyKey = '-----BEGIN PUBLIC KEY-----\n' +
+      'MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQDhELQrtgj6aE81F8o74lOFg7l6\n'+
+      'bUZqe4VQe0MURr0G0zh/hY/KIIxoYfYWBQMwONy0vV23O5XKJDpAJUBHs92mJdB3\n'+
+      'lk95/RsqP0TCpKikrEySLOz9Kfzbf5VWQLRtP4ANfQZbc5K5yrN9Y5D7Ocl2m7pw\n'+
+      '7g9TLkJT1Ue+Mg+kYwIDAQAB\n'+
+      '-----END PUBLIC KEY-----';
+    // appKey = 'yh3SljbeMwGzu0w0wF10TYJ30r49XOxv'
+    var data = new Buffer(0);
+    var result = 'failed';
+    request.on('data', function (chunk) { data = Buffer.concat([data, chunk]); });
+    request.on('end', function (chunk) {
+      data = 'pay?'+data.toString();
+      var out = urlLib.parse(data, true).query;
+      if (out.notify_data) {
+        var cipher = rsaLib.createPublicKey(kyKey);
+        var info = cipher.publicDecrypt(new Buffer(out.notify_data, 'base64'), null, 'utf8');
+        info = urlLib.parse('pay?'+info, true).query;
+        if (info.payresult == 0) {
+          var receipt = info.dealseq;
+          var receiptInfo = unwrapReceipt(receipt);
+          var serverName = 'Master'; //TODO:多服的情况?
+          dbWrapper.updateReceipt(receipt, RECEIPT_STATE_AUTHORIZED, function (err) {
+            dbLib.deliverMessage(receiptInfo.name, {
+              type: MESSAGE_TYPE_ChargeDiamond,
+              paymentType: 'KY',
+              receipt: receipt
+            }, function (err, messageID) {
+              dbWrapper.updateReceipt(receipt, RECEIPT_STATE_DELIVERED, function () {});
+            }, serverName);
+
+            if (err === null) {
+              logInfo({action: 'AcceptPayment', receipt: receipt, info: info});
+              result = 'success';
+            } else {
+              logError({action: 'AcceptPayment', error:err, receipt: receipt, info: info});
+            }
+          });
+        }
+      }
+      response.end(result);
+      data = null;
+    });
+    request.on('error', function (err) {
+      logError({action: 'AcceptPayment', error:err, data: data});
+      data = null;
+      response.end('fail');
+    });
+  }
+}
+
 if (config) {
   initiateFluentLogger();
   initServer();
@@ -80,140 +215,7 @@ if (config) {
     cryptoLib = require('crypto');
     rsaLib = require('ursa');
 
-    paymentServer = require('http').createServer(wrapCallback(function (request, response) {
-      if (request.url.substr(0, 5) === '/pay?') {
-    key = '-----BEGIN PUBLIC KEY-----\n' +
-    'MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEArjB/MQESS9k2Xejv0yN8\n'+
-    't3qQ+IO8UqRTNeqgE4GoPBzmyPY4XJVcijAm7eHkUaF6yxGDh8D03R/yhrQBSdJ1\n'+
-    'GHLEew3KH3VgKNigdN9LGfJuH+k6JgCHwVK+diEQCzkhF6D7qrDvLNkF5iNQr2D+\n'+
-    '3lDAoAhE/PbYvJTH6KGIwx+TJtnNjJMhdE8WmWY5/3OAgyFU6DwVJs4phYWwZsYY\n'+
-    '+Z8s6sq7xKjYeCS0vVonNZ6m9VnYhv1NknxbHDovOMY3Ix/hsu+g2YiJFuUlyyaR\n'+
-    'bM4NUbZfu9OcfAGMo0L2vl6x/x7WzWBRKIGO7ONELfeit3PEPxE0kBvunAMPkQb5\n'+
-    'cwIDAQAB\n'+
-    '-----END PUBLIC KEY-----';
-        var data = new Buffer(0);
-        request.on('data', function (chunk) { data = Buffer.concat([data, chunk]); });
-        request.on('end', function (chunk) {
-          data = 'pay?'+data.toString();
-          var out = urlLib.parse(data, true).query;
-          if (out.sign) {
-            var cipher = rsaLib.createPublicKey(key);
-            var info = cipher.publicDecrypt(new Buffer(out.sign, 'base64'), null, 'utf8');
-            info = JSON.parse(info);
-            if (out.order_id === info.order_id && out.amount === info.amount) {
-              var receipt = info.billno;
-              var receiptInfo = unwrapReceipt(receipt);
-              var serverName = 'Master'; //TODO:多服的情况?
-              dbWrapper.updateReceipt(receipt, RECEIPT_STATE_AUTHORIZED, function (err) {
-                dbLib.deliverMessage(receiptInfo.name, {
-                  type: MESSAGE_TYPE_ChargeDiamond,
-                  paymentType: 'PP25',
-                  receipt: receipt
-                }, function (err, messageID) {
-                  dbWrapper.updateReceipt(receipt, RECEIPT_STATE_DELIVERED, function () {});
-                }, serverName);
-
-                if (err === null) {
-                  logInfo({action: 'AcceptPayment', receipt: receipt, info: info});
-                  return response.end('success');
-                } else {
-                  logError({action: 'AcceptPayment', error:err, data: data});
-                  response.end('fail');
-                }
-              });
-            }
-          }
-          data = new Buffer(0);
-        });
-        request.on('error', function (err) {
-          logError({action: 'AcceptPayment', error:err, data: data});
-          response.end('fail');
-        });
-      } else if (request.url.substr(0, 5) === '/911?') {
-        out = urlLib.parse(request.url, true).query;
-        var appKey = 'd30d9f0f53e2654274505e25c27913fe709eb1ad6265e5c5';
-        var sign = out.AppId+out.Act+out.ProductName+out.ConsumeStreamId+out.CooOrderSerial+
-            out.Uin+out.GoodsId+out.GoodsInfo+out.GoodsCount+out.OriginalMoney+out.OrderMoney+
-            out.Note+out.PayStatus+out.CreateTime+appKey;
-        var b = new Buffer(1024);
-        var len = b.write(sign);
-        sign = md5Hash(b.toString('binary', 0, len));
-        if (sign === out.Sign) {
-          var receipt = out.CooOrderSerial;
-          var receiptInfo = unwrapReceipt(receipt);
-          var serverName = 'Master'; //TODO:多服的情况?
-          dbWrapper.updateReceipt(receipt, RECEIPT_STATE_AUTHORIZED, function (err) {
-            dbLib.getPlayerNameByID(receiptInfo.id, serverName, function (err, name) {
-              dbLib.deliverMessage(name, {
-                type: MESSAGE_TYPE_ChargeDiamond,
-                paymentType: 'ND91',
-                receipt: receipt
-              }, function (err, messageID) {
-                dbWrapper.updateReceipt(receipt, RECEIPT_STATE_DELIVERED, function () {});
-              }, serverName);
-              if (err === null) {
-                logInfo({action: 'AcceptPayment', receipt: receipt, info: out, receiptInfo: receiptInfo, name: name});
-                return response.end('{"ErrorCode": "1", "ErrorDesc": "OK"}');
-              } else {
-                logError({action: 'AcceptPayment', error:err, info: out, receiptInfo: receiptInfo, name: name});
-                return response.end('{"ErrorCode": "0", "ErrorDesc": "Fail"}');
-              }
-            });
-         });
-        } else {
-          logError({action: 'AcceptPayment', error: 'SignMissmatch', info: out, sign: sign});
-          response.end('{"ErrorCode": "5", "ErrorDesc": "Fail"}');
-        }
-      } else if (request.url.substr(0, 4) === '/kyp') {
-        var kyKey = '-----BEGIN PUBLIC KEY-----\n' +
-          'MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQDhELQrtgj6aE81F8o74lOFg7l6\n'+
-          'bUZqe4VQe0MURr0G0zh/hY/KIIxoYfYWBQMwONy0vV23O5XKJDpAJUBHs92mJdB3\n'+
-          'lk95/RsqP0TCpKikrEySLOz9Kfzbf5VWQLRtP4ANfQZbc5K5yrN9Y5D7Ocl2m7pw\n'+
-          '7g9TLkJT1Ue+Mg+kYwIDAQAB\n'+
-          '-----END PUBLIC KEY-----';
-        // appKey = 'yh3SljbeMwGzu0w0wF10TYJ30r49XOxv'
-        var data = new Buffer(0);
-        request.on('data', function (chunk) { data = Buffer.concat([data, chunk]); });
-        request.on('end', function (chunk) {
-          data = 'pay?'+data.toString();
-          var out = urlLib.parse(data, true).query;
-          if (out.notify_data) {
-            var cipher = rsaLib.createPublicKey(kyKey);
-            var info = cipher.publicDecrypt(new Buffer(out.notify_data, 'base64'), null, 'utf8');
-            info = urlLib.parse('pay?'+info, true).query;
-            if (info.payresult === 0) {
-              var receipt = info.dealseq;
-              var receiptInfo = unwrapReceipt(receipt);
-              var serverName = 'Master'; //TODO:多服的情况?
-              dbWrapper.updateReceipt(receipt, RECEIPT_STATE_AUTHORIZED, function (err) {
-                dbLib.deliverMessage(receiptInfo.name, {
-                  type: MESSAGE_TYPE_ChargeDiamond,
-                  paymentType: 'KY',
-                  receipt: receipt
-                }, function (err, messageID) {
-                  dbWrapper.updateReceipt(receipt, RECEIPT_STATE_DELIVERED, function () {});
-                }, serverName);
-
-                if (err === null) {
-                  logInfo({action: 'AcceptPayment', receipt: receipt, info: info});
-                  return response.end('success');
-                } else {
-                  logError({action: 'AcceptPayment', error:err, data: data});
-                  response.end('failed');
-                }
-              });
-            }
-          }
-          response.end('failed');
-          data = null;
-        });
-        request.on('error', function (err) {
-          logError({action: 'AcceptPayment', error:err, data: data});
-          data = null;
-          response.end('fail');
-        });
-      }
-    }));
+    paymentServer = require('http').createServer(wrapCallback(paymentHandler));
     paymentServer.listen(6499);
   });
 } else {
